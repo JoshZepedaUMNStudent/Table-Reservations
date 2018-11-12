d63ec3e10a318d9dde160cab6ca7fcd0141bd1f1
JZStudent <joshzepeda.student@gmail.com>
2018-11-12 16:35:33 -0600

    finalized

--- Main/Routes/htmlRoutes.js
+++ Main/Routes/htmlRoutes.js
@@ -3,14 +3,14 @@ var path = require("path");
 module.exports = function(app) {
 
     app.get("/tables", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/tables.html"));
+        res.sendFile(path.join(_dirname, "public/tables.html"));
     });
 
     app.get("/reserve", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/reserve.html"));
+        res.sendFile(path.join(_dirname, "public/reserve.html"));
     });
 
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(_dirname, "public/home.html"));
     });
 };
