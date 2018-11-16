b3c7617bff4e665552af83af865945e1087f1d8d
JZStudent <joshzepeda.student@gmail.com>
2018-11-15 21:36:21 -0600

    1

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("routes/apiRoutes")(app);
-require("routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes.html")(app);
+require("./app/routes/htmlRoutes.html")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
